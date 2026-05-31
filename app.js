aaedf4883149c2e16b3f050b98e48898a38e8688
Boredest <19357608+Boredest@users.noreply.github.com>
2026-05-31 19:13:07 +0900

    Updated order of buttons. I think the UI is good enough that I can focus on JS for the rest of the project now.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const container = document.querySelector(".container");
 
 const buttonsContainer= document.querySelector(".buttons-container");
 
-const buttonArray = ["0","1","2","3","4","5","6","7","8","9","+","-","*","/","=","."];
+const buttonArray = ["7","8","9","/","4","5","6","*","1","2","3","-","0",".","=","+"];
 
 console.log(buttonsContainer);
 
