81957afdd1d510330a697905663ff745f45fb0dc
Boredest <19357608+Boredest@users.noreply.github.com>
2026-06-02 22:14:15 +0900

    Updated JS. The calculator is not working, but the buttons now reflect what the user clicks. It needs so it can store the proper state and call the right functions.

--- app.js
+++ app.js
@@ -1,14 +1,69 @@
-const container = document.querySelector(".container");
+const calculatorContainer = document.querySelector(".calculator");
 
-const buttonsContainer= document.querySelector(".buttons-container");
+const buttonsContainer = document.querySelector(".buttons-container");
 
-const buttonArray = ["7","8","9","/","4","5","6","*","1","2","3","-","0",".","=","+"];
+const screen = document.querySelector(".screen");
 
-console.log(buttonsContainer);
+let input = "";
 
-buttonArray.forEach(function(button) {
+const buttonArray = [
+  "7",
+  "8",
+  "9",
+  "/",
+  "4",
+  "5",
+  "6",
+  "*",
+  "1",
+  "2",
+  "3",
+  "-",
+  "0",
+  ".",
+  "=",
+  "+",
+];
+
+function generateCalculatorGrid() {
+  buttonArray.forEach(function (button) {
     const buttonElement = document.createElement("button");
     buttonElement.innerText = button;
     buttonElement.classList.add("btn");
     buttonsContainer.appendChild(buttonElement);
+  });
+}
+
+calculatorContainer.addEventListener("mousedown", (event) => {
+  let target = event.target;
+
+  if (!target.matches(".btn")) return;
+  console.log(event.target);
+  if (target.matches(".btn.clear-btn")) {
+    console.log("CLEAR");
+    screen.innerText = "0";
+    input = "";
+  } else {
+    input += event.target.innerText;
+    screen.innerText = input;
+  }
 });
+
+generateCalculatorGrid();
+
+function add(x, y) {
+  console.log(x + y);
+  return x + y;
+}
+
+function subtract() {}
+
+function multiply() {}
+
+function divide() {}
+
+function operate(operator, x, y) {}
+
+function updateDisplay(result) {
+  screen.innerText = result;
+}
